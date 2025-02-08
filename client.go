c135016e2240c06219c36da0f18a1d147f1b4651
RajVerma97 <rajneeskumar2545@gmail.com>
2025-02-09 00:10:23 +0530

    add heartbeat ping and pong

--- client.go
+++ client.go
@@ -3,8 +3,14 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+var (
+	pongWait     = 10 * time.Second
+	pingInterval = (pongWait * 9) / 10
 )
 
 type ClientList map[*Client]bool
@@ -32,6 +38,12 @@ func (c *Client) ReadMessages() {
 		c.manager.removeClient(c)
 	}()
 
+	if err := c.connection.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		log.Println(err)
+		return
+	}
+
+	c.connection.SetPongHandler(c.pongHandler)
 	for {
 		_, payload, err := c.connection.ReadMessage()
 		if err != nil {
@@ -61,6 +73,7 @@ func (c *Client) WriteMessages() {
 	defer func() {
 		c.manager.removeClient(c)
 	}()
+	ticker := time.NewTicker(pingInterval)
 	for {
 		select {
 		//backend:Read from the egress Channel:
@@ -85,6 +98,21 @@ func (c *Client) WriteMessages() {
 				log.Printf("failed to send message: %v", err)
 			}
 			log.Println("message sent")
+
+		case <-ticker.C:
+			log.Println("ping")
+
+			//send a ping to the client
+			if err := c.connection.WriteMessage(websocket.PingMessage, []byte(``)); err != nil {
+				log.Println("write msg err:", err)
+				return
+			}
 		}
 	}
+}
+
+func (c *Client) pongHandler(pongMsg string) error {
+	log.Println("pong")
+	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
+
 }
